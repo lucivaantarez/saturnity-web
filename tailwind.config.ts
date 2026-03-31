de4eda5401213b7894076283b9e2e89d6a028bba
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-01 00:40:05 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -25,4 +24,3 @@ const config: Config = {
   plugins: [],
 };
 export default config;
-
