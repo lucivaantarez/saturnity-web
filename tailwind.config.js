eb8f743571cbe30e17831e6fd6ce86dbfdb3fc13
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-04-01 00:44:10 +0700

    Update and rename tailwind.config.ts to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -23,4 +22,3 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
